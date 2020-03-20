b3284ddf02ef1fce548d245683cc511e093820d2
krsteen <kristoffer.steen@hotmail.com>
2020-03-20 07:29:57 +0100

    Initial setup

---
+++ EFCorePerformance.Cmd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCorePerformance.Cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
