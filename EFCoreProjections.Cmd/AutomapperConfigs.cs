80556ae632ceeed9a842a558eaa015303682bf74
Kristoffer Steen <KRST@equinor.com>
2022-11-03 13:45:25 +0100

    Automapper

--- EFCoreProjections.Cmd/AutomapperConfigs.cs
+++ EFCoreProjections.Cmd/AutomapperConfigs.cs
@@ -10,21 +10,11 @@ namespace EFCoreProjections.Cmd
 
             CreateMap<Report, ReportDto>();
 
-            CreateMap<ReportDapper, ReportDto>();
-
             CreateMap<Report, ReportListItemDto>();
-
-
 
             CreateMap<ReportConfig, ReportConfigDto>();
 
-            CreateMap<ReportConfigDapper, ReportConfigDto>();
-
-
-
             CreateMap<ReportComment, ReportCommentDto>();
-
-            CreateMap<ReportCommentDapper, ReportCommentDto>();
         }
 
     }
